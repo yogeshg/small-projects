8ffa7e6b60b5d2d3757b0abc9ab444c590222a45
Yogesh Garg <yg2482@delhi.clic.cs.columbia.edu>
2016-11-04 11:25:18 -0400

    adding confirmation to script

--- profiles/setup_user.sh
+++ profiles/setup_user.sh
@@ -1,10 +1,36 @@
-set -x
-ssh-keygen -q
-cat $PROFILES/id_rsa.pub >> $HOME/.ssh/authorized_keys
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case $response in
+        [yY][eE][sS]|[yY])
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
 
-cat $PROFILES/vimrc >> $HOME/.vimrc
-cat $PROFILES/gitconfig.cfg >> $HOME/.gitconfig
-cat $PROFILES/bashrc.sh >> $HOME/.bashrc
-cat $PROFILES/bash_aliases.sh >> $HOME/.bash_aliases
-echo 'source $HOME/.bashrc' >> .profile
+set -x
+
+echo 'generatind ssh key'
+confirm && ssh-keygen -q
+
+echo 'granting ssh access'
+confirm && cat $PROFILES/id_rsa.pub >> $HOME/.ssh/authorized_keys
+
+echo 'appending vimrc'
+confirm && cat $PROFILES/vimrc >> $HOME/.vimrc
+
+echo 'appending gitconfig'
+confirm && cat $PROFILES/gitconfig.cfg >> $HOME/.gitconfig
+
+echo 'appending bashrc'
+confirm && cat $PROFILES/bashrc.sh >> $HOME/.bashrc
+
+echo 'appending bash_aliases.sh'
+confirm && cat $PROFILES/bash_aliases.sh >> $HOME/.bash_aliases
+
+echo 'sourcing bashrc in profile'
+confirm && echo 'source $HOME/.bashrc' >> $HOME/.profile
 
