71385aa67237e6d6737e966600b44bedfef4f990
Yogesh Garg <yg2482@columbia.edu>
2017-02-14 15:45:43 -0500

    sample3.3

--- cpp/hw4/hw4-sample3.c
+++ cpp/hw4/hw4-sample3.c
@@ -1,11 +1,17 @@
 #include"stdio.h"
+#include"stdlib.h"
 int main() {
-	int arr[] = {100,200,300,400,500};
+	int* arr;
+	arr = malloc(sizeof(arr) * 10);
 	int i;
+	for( i=0; i<10; ++i ){
+		arr[i] = 100*i;
+	}
 	for( i=1; i<10; i*=2){
 		printf("%d ",arr[i]);		// potential for invalid access
 	}
 	printf("\n");
+	free(arr);
 	return 0;
 }
 
