8d603c4abb9121c2de1dcb8f9b6d10b424a87860
Yogesh Garg <yg2482@columbia.edu>
2017-03-06 22:26:45 +0000

    shared pointers for graph and edges

--- cpp/hw5/graph.cpp
+++ cpp/hw5/graph.cpp
@@ -1,7 +1,52 @@
+#include"graph.h"
+#include<vector>
+#include<cassert>
+#include<memory>
 
-#include<graph.h>
+using namespace graph;
+
+typedef Value Vertex;
+typedef std::shared_ptr<Vertex> Vertex_ptr;
+typedef std::pair<Vertex_ptr, Vertex_ptr> Edge;
+typedef std::shared_ptr<Edge> Edge_ptr;
+
+class Adjacency_matrix {
+    public:
+        Adjacency_matrix() {}
+
+        std::vector<std::vector<Edge_ptr>> m;        // actaual matrix
+        std::vector<Vertex_ptr> vertices;       // list of vertices
+
+        void add(Vertex_ptr x) {
+            std::vector<Edge_ptr> row;
+            // New vertex has no edge for any previous vertex
+            for(int i=0; i<vertices.size(); ++i) {
+                Edge_ptr null_edge_ptr;
+                row.push_back( null_edge_ptr );               // TODO push null pointer
+            }
+            m.push_back(row);
+
+            // Old vertices have no edge for new vertex
+            for(auto r : m) {
+                Edge_ptr null_edge_ptr;
+                r.push_back( null_edge_ptr );
+            }
+
+            // add vertex to the list
+            vertices.push_back(x);            
+        }
+        // void add_edge(Edge_ptr e) {
+
+        // }
+};
+
+void add(Adjacency_matrix& g, Vertex_ptr x) {
+    
+}
 
 int main() {
+    Adjacency_matrix a;
+    auto pV1 = std::make_shared<Vertex>;
     return 0;
 }
 
--- cpp/hw5/graph.h
+++ cpp/hw5/graph.h
@@ -1,27 +1,30 @@
 #include<string>
-#include<pair>
+#include<utility>
 
-typedef std::pair<std::string,int> Value;
+namespace graph {
 
-//    bool adjacent(Graph& g, Vertex_ptr x, Vertex_ptr y);
-//    bool neighbors(Graph& g, Vertex_ptr x);
-//    void add(Graph& g, Vertex_ptr x);
-//    void remove(Graph& g, Vertex_ptr x);
-//    void add_edge(Grap&h g, Vertex_ptr x, Vertex_ptr y);
-//    void add_edge(Graph& g, Edge_ptr e); // and edge is a pair of vertexes
-//    value value(Graph& g, Vertex_ptr x); // vertex value
-//    void set_value(Graph& g, Vertex x, Value v); // set vertex value
-//    Value value(Graph& g, Edge_ptr e); // edge value
-//    void set_value(Graph& g, Edge_ptr e, Value v); // edge value
-//    Vertex top(Graph&); // get some Vertex
+    typedef std::pair<std::string,int> Value;
+
+    //    bool adjacent(Graph& g, Vertex_ptr x, Vertex_ptr y);
+    //    bool neighbors(Graph& g, Vertex_ptr x);
+    //    void add(Graph& g, Vertex_ptr x);
+    //    void remove(Graph& g, Vertex_ptr x);
+    //    void add_edge(Grap&h g, Vertex_ptr x, Vertex_ptr y);
+    //    void add_edge(Graph& g, Edge_ptr e); // and edge is a pair of vertexes
+    //    value value(Graph& g, Vertex_ptr x); // vertex value
+    //    void set_value(Graph& g, Vertex x, Value v); // set vertex value
+    //    Value value(Graph& g, Edge_ptr e); // edge value
+    //    void set_value(Graph& g, Edge_ptr e, Value v); // edge value
+    //    Vertex top(Graph&); // get some Vertex
 
 
 
-//    Write a function that prints a graph in some format (e.g., as text)
-//    Write a function that counts the number of vertexes
-//    Write a function that counts the number of edges
-//    Write a function that check if every vertex is reachable from the top
-//    Write a function that finds cycles
-//    Write a function that lists the vertexes in order of Values (highest value first)
-//    Write a function that lists the edges in order of Values (highest value first)
+    //    Write a function that prints a graph in some format (e.g., as text)
+    //    Write a function that counts the number of vertexes
+    //    Write a function that counts the number of edges
+    //    Write a function that check if every vertex is reachable from the top
+    //    Write a function that finds cycles
+    //    Write a function that lists the vertexes in order of Values (highest value first)
+    //    Write a function that lists the edges in order of Values (highest value first)
 
+}
