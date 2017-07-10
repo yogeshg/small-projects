4f23caf88225a9f0633400f8862756a75248817d
Yogesh Garg <yogeshg91@gmail.com>
2017-07-10 18:15:26 +0000

    add more aliases

--- profiles/bash_aliases.sh
+++ profiles/bash_aliases.sh
@@ -1,4 +1,15 @@
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
 alias les='less -S'
 alias csvv='column -s, -t'
 alias psvv='column -s\| -t'
+
+function timestamp() { date +"%Y%m%d_%H%M%S"; }
+function hr() { printf -v buff "%-$(tput cols)s" " "; echo "${buff// /${1:-"-"}}"; }
 
