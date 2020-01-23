7d420641ab7f34957935043e97b24a2d6e78730e
yogesh garg <yo@Yogeshs-MacBook-Pro.local>
2020-01-22 23:14:50 -0700

    add a history file with useful things

---
+++ profiles/bash_history.sh
@@ -0,0 +1,159 @@
+mkdir ~/.bash_sessions_disable
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew doctor
+brew cask install firefox
+brew install spectacle
+brew install tmux
+brew cask install docker dropbox google-drive sublime-text spectacle 1password vlc
+brew cask install slack
+mkdir -p $HOME/Desktop/screenshots/
+defaults write com.apple.screencapture location $HOME/Desktop/screenshots/ && killall SystemUIServer
+brew cask install qlmarkdown quicklook-csv betterzip
+brew install tree
+brew install git
+mkdir ~/bin/
+mkdir ~/code/
+vim ~/Downloads/hub-darwin-amd64-2.11.2.tgz 
+tar -xvzf ~/Downloads/hub-darwin-amd64-2.11.2.tgz 
+mv hub-darwin-amd64-2.11.2/ bin/
+ln -s /Users/yo/bin/hub-darwin-amd64-2.11.2/bin/hub /Users/yo/bin/
+ll ~/bin/
+ls ~/bin/
+ls -larth ~/bin/
+which hub
+export PATH=$PATH:$HOME/bin
+which hub
+cd ~/code/
+hub clone yogeshg/small-projects
+cd ~/code/small-projects/profiles/
+./setup_user.sh 
+cd ~/
+
+
+mkdir ~/.bash_sessions_disable
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew doctor
+brew cask install firefox
+brew install spectacle
+brew install tmux
+brew cask install docker dropbox google-drive sublime-text spectacle 1password vlc
+brew cask install slack
+mkdir -p $HOME/Desktop/screenshots/
+defaults write com.apple.screencapture location $HOME/Desktop/screenshots/ && killall SystemUIServer
+brew cask install qlmarkdown quicklook-csv betterzip
+brew install tree
+brew install git
+mkdir ~/bin/
+mkdir ~/code/
+vim ~/Downloads/hub-darwin-amd64-2.11.2.tgz 
+tar -xvzf ~/Downloads/hub-darwin-amd64-2.11.2.tgz 
+mv hub-darwin-amd64-2.11.2/ bin/
+ln -s /Users/yo/bin/hub-darwin-amd64-2.11.2/bin/hub /Users/yo/bin/
+ll ~/bin/
+ls ~/bin/
+ls -larth ~/bin/
+which hub
+export PATH=$PATH:$HOME/bin
+which hub
+cd ~/code/
+hub clone yogeshg/small-projects
+cd ~/code/small-projects/profiles/
+./setup_user.sh 
+cd ~/
+ls -larth 
+ls -larth .bash_sessions/
+ls -larth .bash_sessions/*.history
+cat .bash_sessions/*.history
+vim -p .bash_sessions/*
+l
+vim ~/.bash_history 
+brew install bash-completion
+git config --global user.name "yogesh garg"
+git config --global user.email "yogesh@starskyrobotics.com"
+git config --global credential.helper osxkeychain
+pbcopy < ~/.ssh/id_rsa.pub
+brew install bash-completion
+echo "[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion" >> ~/.bash_dev 
+source ~/.bash_dev 
+vim /Users/yo/.bash_history 
+brew search completion
+brew install docker-completion
+brew install vim
+brew cask install docker dropbox sublime-text spectacle 1password vlc
+subl 
+brew install python
+pip install virtualenv
+which pip
+curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
+sudo python get-pip.py
+which pip
+pip --version
+sudo python3 get-pip.py
+which -a pip
+which -a pip pip2 pip3
+which -a pip pip2 pip3 | xargs ls -larth 
+pip install virtualenv
+pip install virtualenvwrapper
+which -a python python2 python3
+brew install python@2
+which -a python python2 python3
+which python
+which -a python
+which -a python python2 python3
+which virtualenv
+which virtualenv_wrapper
+which virtualenvwrapper
+which virtualenvwrapper.sh 
+brew --prefix
+vim /Users/yo/.bash_dev 
+subl
+caty ~/code/small-projects/profiles/sublime-preferences.json 
+cat ~/code/small-projects/profiles/sublime-preferences.json 
+which -a python python2 python3
+source ~/.bash_dev 
+brew --prefix
+vim /Users/yo/.bash_dev 
+echo $VIRTUALENVWRAPPER_PYTHON 
+which virtualenvwrapper.sh 
+vim /Users/yo/.bash_dev 
+which virtualenvwrapper.sh 
+/usr/local/bin/virtualenvwrapper.sh 
+VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2 /usr/local/bin/virtualenvwrapper.sh 
+PATH=$PATH:"/usr/local/bin" VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2 /usr/local/bin/virtualenvwrapper.sh 
+PATH="/usr/local/bin":$PATH VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2 /usr/local/bin/virtualenvwrapper.sh 
+which python
+python --version
+vim /Users/yo/.bash_dev 
+workon dev2
+mkvirtualenv dev2 --python=python2
+mkvirtualenv dev3 --python=python3
+which python
+which -a python
+ls -larth /usr/local/bin/python
+ls -larth /usr/local/Cellar/python@2/2.7.16/bin/python
+/usr/local/Cellar/python@2/2.7.16/bin/python
+/usr/local/Cellar/python@2/2.7.16/bin/pip
+/usr/local/Cellar/python@2/2.7.16/bin/pip install virtualenv
+/usr/local/Cellar/python@2/2.7.16/bin/pip install virtualenvwrapper
+vim /Users/yo/.bash_dev 
+workon dev2
+deactivate 
+mkvirtualenv dev3 --python=python3
+mkvirtualenv dev2 --python=python2
+which -a python pip
+which python pip
+cd ~/code/
+l
+rew install gcc
+brew install gcc
+which pip
+pip install ipython
+docker run hello-world
+which docker
+brew cask install docker #dropbox sublime-text spectacle 1password vlc
+which docker
+docker run hello-world
+
+
