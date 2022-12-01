2d594831e7ddba092f299c5a987c1eab397ecf98
yogesh garg <1059168+yogeshg@users.noreply.github.com>
2022-12-01 15:54:50 -0800

    better

--- streamlit_apps/github_repo_stats/app.py
+++ streamlit_apps/github_repo_stats/app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import random
 import requests
+import datetime as dt
 
 SAMPLE_REPOS = [
-    "wandb/wandb",
     "wandb/local",
     "pytorch/pytorch",
     "keras-team/keras",
@@ -12,6 +12,9 @@ SAMPLE_REPOS = [
     "minio/minio",
     "neithere/argh",
 ]
+
+def parse(strtime):
+    return dt.datetime.strptime(strtime, "%Y-%m-%dT%H:%M:%SZ")
 
 def main(st):
     all_data = []
@@ -39,32 +42,37 @@ def main(st):
 def sample_repos():
     return random.sample(SAMPLE_REPOS, 3)
 
-
 @st.cache(ttl=600)
+def page_and_parse(gh_api_request):
+    data = []
+    for page_idx in range(100):
+        resp = requests.get(gh_api_request + f"&per_page=100&page={page_idx+1}")
+        if not resp:
+            err = f"error in making request: {resp.text}"
+            return data, err
+        rc = 0
+        for row in resp.json():
+            rc += 1
+            created_at, url = row.get("created_at"), row.get("html_url")
+            data.append((created_at, url))
+        if rc < 100:
+            break
+    return data, None
+
+
 def crawl_repo(repo_name):
 
     print(f"making requests for {repo_name}")
-    issues = requests.get(f"https://api.github.com/repos/{repo_name}/issues?filter=all&state=all&sort=created")
-    releases = requests.get(f"https://api.github.com/repos/{repo_name}/releases")
 
-    if not issues or not releases:
-        err = f"error in issues or releases request:\n{issues.text}\n{releases.text}"
-        data = [dict(repo_name=repo_name, created_at=None, issue_count=None, release_count=None, url=None)]
-        print(err)
-        return data, err
+    issues, issues_err = page_and_parse(f"https://api.github.com/repos/{repo_name}/issues?filter=all&state=all&sort=created")
+    releases, releases_err = page_and_parse(f"https://api.github.com/repos/{repo_name}/releases")
 
     all_data = []
-
-    for row in issues.json():
-        # print(f"row: {row}")
-        created_at, url = row.get("created_at"), row.get("html_url")
+    for created_at, url in issues:
         all_data.append((created_at, 1, 0, url))
-
-    for row in releases.json():
-        # print(f"row: {row}")
-        created_at, url = row.get("created_at"), row.get("html_url")
+    for created_at, url in releases:
         all_data.append((created_at, 0, 1, url))
-    
+
     all_data.sort(key=lambda row: row[0])
 
     processed_data = []
@@ -75,13 +83,24 @@ def crawl_repo(repo_name):
         release_count += release_incr
         processed_data.append(dict(
             repo_name=repo_name,
-            created_at=created_at,
+            created_at=parse(created_at),
             issue_count=issue_count,
             release_count=release_count,
             url=url
         ))
+    if len(processed_data) == 0:
+        processed_data.append(dict(
+            repo_name=repo_name,
+            created_at="2222-12-12T22:22:22Z",
+            issue_count=0,
+            release_count=0,
+            url=""
+        ))
 
-    return processed_data, None
+    return processed_data, issues_err or releases_err
+
 
 main(st)
+
+
 
