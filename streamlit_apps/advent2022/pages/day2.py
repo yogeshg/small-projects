5c9acca2abca933d8ee34e08e532e5fe0119f0a7
Yogesh Garg <yogeshg91@gmail.com>
2022-12-01 21:18:04 -0800

    p2

--- streamlit_apps/advent2022/pages/day2.py
+++ streamlit_apps/advent2022/pages/day2.py
@@ -18,7 +18,29 @@ def is_win(they, you):
 def value(you):
     return dict(X=1,Y=2,Z=3)[you]
 
-def func(inp):
+def is_win2(they, outcome):
+    return dict(X=0, Y=3, Z=6)[outcome]
+
+def value2(they, outcome):
+    return dict(
+        A=dict(
+            X=3,  # rock, loss -> scissor -> 3
+            Y=1,  # rock, draw -> rock -> 1
+            Z=2,  # rock, win -> paper -> 2
+            ),
+        B=dict(
+            X=1,  # paper, loss -> rock -> 1
+            Y=2,  # paper, draw -> paper -> 2
+            Z=3,  # paper, win -> sciros -> 3
+            ),
+        C=dict(
+            X=2,  # scissor, loss -> paper -> 2
+            Y=3,  # scissor, draw -> scissor -> 3
+            Z=1,  # scissor, win -> rock -> 1
+            ),
+    )[they][outcome]
+
+def func1(inp):
     score = 0
     for line in inp.split("\n"):
         they, you = line.split()
@@ -32,13 +54,27 @@ def func(inp):
         a2=0,
     )
 
+def func2(inp):
+    score = 0
+    for line in inp.split("\n"):
+        they, you = line.split()
+        score_round = is_win2(they, you)
+        score_shape = value2(they, you)
+        curr_score = score_shape + score_round
+        score += curr_score
+
+    return dict(
+        a1=0,
+        a2=score,
+    )
+
 # score_shape = 1, 2, 3
 # score_round = 0, 3, 6
 # 
 
 def main(st):
     inp = st.text_area("input")
-    out = func(inp)
+    out = func2(inp)
     st.write(out)
 
 
