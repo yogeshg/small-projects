435af1a98ef9fbdb1dc6b056514fbd82f8d38287
Yogesh Garg <yogeshg91@gmail.com>
2025-08-30 22:04:21 -0700

    Add SF-Permits

---
+++ docs/sf-permits/_next/static/F4XjMsIntY67vY-FuRWwt/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/ebfe8c2072b24197.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ docs/sf-permits/_next/static/chunks/103c061b65487c79.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/49aa3cbf5f890f06.js",
+  "static/chunks/turbopack-3ad345c99dc663e1.js"
+])
